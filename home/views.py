5016aad4330193e3def2fd1d1ee1703629b2bf0f
Suman Sunuwar <jaycessunuwar@gmail.com>
2020-11-05 18:23:21 +0545

    loginrequired decorator added

--- home/views.py
+++ home/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, reverse, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from home.models import Todo
 from home.forms import TodoForm
+import datetime
+from datetime import date
 
-
+@login_required(login_url='/login/')
 def home_view(request):
-	todos = Todo.objects.all()
-	context = {"todos" : todos, "user": request.user}
+	todo = Todo.objects.all()
+	context = {"todos" : todo, "user": request.user, "todays_date" : datetime.date.today()}
 	return render(request, "todo_list.html", context)
 
+@login_required(login_url='/login/')
 def add_todo(request):
 	form = TodoForm(request.POST or None, request.FILES or None)
 	context = {"form" : form}
@@ -17,6 +21,7 @@ def add_todo(request):
 		return HttpResponseRedirect(reverse('home:todo_list'))
 	return render(request, "form.html", context)
 
+@login_required(login_url='/login/')
 def update_todo(request,todo_id):
 	todo = get_object_or_404(Todo,id=todo_id)
 	form = TodoForm(request.POST or None, request.FILES or None, instance=todo)
@@ -25,6 +30,7 @@ def update_todo(request,todo_id):
 		return HttpResponseRedirect(reverse('home:todo_list'))
 	return render(request, "form.html", {"form" : form})
 
+@login_required(login_url='/login/')
 def delete_todo(request, todo_id):	# passing the request and POST from the delete key in indext.html
 	todo = get_object_or_404(Todo, id=todo_id) 	# getting the item ready which was requested with POST method
 	if request.method == 'POST':	# this POST is confirm button in the delete.html page which passes through request 
