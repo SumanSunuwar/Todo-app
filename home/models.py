066aa3b2969fa0a318ccc4118b4175be819acbbe
Suman Sunuwar <jaycessunuwar@gmail.com>
2020-11-05 18:20:32 +0545

    added fields is_grater_priority and profile which is foreignkey

--- home/models.py
+++ home/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
+from userprofile.models import Profile
 # Create your models here.
 #Let's create an object > Todo object
 # fields : title , description, date
-
 class Todo(models.Model):
 	title = models.CharField(max_length=255)
 	description = models.TextField(null=True, blank=True)
 	date = models.DateField(null=True,blank=True)
 	todo_image = models.ImageField(upload_to="upload/todo",blank=True, null=True)
+	is_greater_priority = models.BooleanField(default=False)
+	profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
 
 	def __str__(self):
 		return self.title	
+ 
