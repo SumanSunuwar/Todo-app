44222bc4ddae1065bc9173854e02166733a3c709
Suman Sunuwar <jaycessunuwar@gmail.com>
2020-11-25 10:08:46 +0545

    no changes

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,8 @@ from home.views import home_view, add_todo, update_todo, delete_todo
 app_name = "home"
 
 urlpatterns = [
-    path('todo_list', home_view, name='todo_list'),
-    path('add_todo', add_todo, name='add_todo'),
+    path('todo_list/', home_view, name='todo_list'),
+    path('add_todo/', add_todo, name='add_todo'),
     path('update_todo/<int:todo_id>/', update_todo, name='update_todo'),
     path('delete_todo/<int:todo_id>/', delete_todo, name='delete_todo'),
 ] 
