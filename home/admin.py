836e266bdbf82e80a0357a97477e10a08cc54e65
Suman Sunuwar <jaycessunuwar@gmail.com>
2020-11-25 10:06:29 +0545

    added user filter listing

--- home/admin.py
+++ home/admin.py
@@ -4,7 +4,8 @@ from home.models import Todo
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-	list_display = ("title", "description", "date", "todo_image", "is_greater_priority", "profile", )
-	list_filter = ("date", "title",)
+	list_display = ("title", "description", "date", "todo_image", "is_greater_priority", "user", )
+	list_filter = ("date", "title", "user",)
 	search_fields = ("title", "date",)
 	
+
