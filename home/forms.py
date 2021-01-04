e6ec2a52ab250c09e63c7b8b8c3d995b54910953
Suman Sunuwar <jaycessunuwar@gmail.com>
2021-01-04 13:36:48 +0545

    latest commit\

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from home.models import Todo
 from django.db.models import fields
 
+
 #class TodoForm(forms.Form):
 #	title = forms.CharField()
 #	description = forms.CharField()
