0456a9cb12bdb708cc72df067474687840c0e44c
Suman Sunuwar <jaycessunuwar@gmail.com>
2021-01-17 12:54:37 +0545

    new html created

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from home.models import Todo
 from django.db.models import fields
+from django.contrib.auth.models import User
+
 
 
 #class TodoForm(forms.Form):
@@ -9,6 +11,19 @@ from django.db.models import fields
 #	date = forms.DateField()
 
 class TodoForm(forms.ModelForm):
+	def __init__(self, *args):
+		self.user = args.pop('username', None)
+		super(TodoForm, self).__init__(*args)
+
+	def save(self, commit=True):
+		obj = super(TodoForm, self).save(commit=False)
+		obj.user = self.user
+		if commit:
+            obj.save()
+        return obj
+
+
+
 	class Meta:
 		model = Todo
-		fields = ("title", "description", "date", "todo_image", "is_greater_priority", "user",)
+		fields = ["title", "description", "date", "todo_image", "is_greater_priority", "user"]
